8bcdbc64127f21c087c22fc4f2ac1a87a036d72f
unknown <megan.buglio@eggharborcafe.com>
2024-05-09 12:52:41 -0500

    another one

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 const http = require('http');
-const fs = require('fs');
 const port = process.env.PORT || 3000;
 
 http.createServer(function(request, response) {
-  if (request.url === '/') {
-    // Serve the HTML file
-    fs.readFile('index.html', function(err, data) {
-      if (err) {
-        response.writeHead(500);
-        response.end('Error loading index.html');
-      } else {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.end(data);
-      }
-    });
-  } else {
-    // Handle other requests
-    // Your other server logic here
-  }
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.end('<h1>Hello World!</h1>');
 }).listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
