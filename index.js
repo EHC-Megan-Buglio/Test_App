29cc63d451e0e35dfa7d9884a74ff0450f8cdff8
unknown <megan.buglio@eggharborcafe.com>
2024-05-10 12:50:00 -0500

    trying out something fancy

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ const port = process.env.PORT || 3000;
 
 http.createServer(function(request, response) {
   response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.end('<h1>Hello World!!</h1> <h2>Egg Harbies Coming at ya!</h2>');
+  response.write('<div id="rotatingText"></div>');
+
+  // JavaScript for rotating text
+  response.write('<script>');
+  response.write(`
+    const rotatingText = document.getElementById('rotatingText');
+    const texts = ['Egg Harbor Cafe', 'Egg Harbor Café', 'Egg Harbor Cafe!'];
+    let index = 0;
+
+    function rotateText() {
+      rotatingText.innerHTML = texts[index];
+      index = (index + 1) % texts.length;
+    }
+
+    setInterval(rotateText, 2000); // Change text every 2 seconds
+  `);
+  response.write('</script>');
+
+  response.end();
 }).listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
